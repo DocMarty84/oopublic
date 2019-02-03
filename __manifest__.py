750648dc3ba6338c10819787fdd1f3c376e49368
Nicolas Martinelli <nicolas.martinelli@pm.me>
2019-02-03 09:57:27 +0100

    Initial commit

---
+++ __manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "OOPublic",
+    "author": "Nicolas Martinelli",
+    "category": "Uncategorized",
+    "summary": "Public access",
+    "website": "https://koozic.net/",
+    "version": "0.1",
+    "description": """
+Public access to backend
+========================
+
+        """,
+    "depends": ["base", "web"],
+    "data": [],
+    "demo": [],
+    "qweb": [],
+    "installable": True,
+    "application": True,
+    "license": "Other OSI approved licence",
+}
---
+++ controllers/main.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+import uuid
+
+from odoo import http
+from odoo.http import request
+from odoo.service import security
+
+
+class PublicController(http.Controller):
+    @http.route("/public", type="http", auth="none", sitemap=False)
+    def public(self):
+        if not request.session.uid:
+            tmp_login = "tmp_{}".format(uuid.uuid4().hex)
+            tmp = (
+                request.env["res.users"]
+                .sudo()
+                .create({"name": "", "login": tmp_login, "password": tmp_login})
+            )
+            request.session.uid = tmp.id
+            request.env["res.users"]._invalidate_session_cache()
+            request.session.session_token = security.compute_session_token(
+                request.session, request.env
+            )
+            return http.local_redirect("web", keep_hash=True)
+        return http.local_redirect("/", query=request.params, keep_hash=True)
